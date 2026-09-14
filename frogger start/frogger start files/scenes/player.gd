05b8dc7e2090e6a6614955632bc44c89c0241b59
Lucas Olvr. <lucasgamerzup@gmail.com>
2026-09-14 14:06:19 -0300

    Aula 5(Animações)
    Adicionei os sprites e animações responsivas do personagem. para andar para direita, esquerda, cima e baixo.

--- frogger start/frogger start files/scenes/player.gd
+++ frogger start/frogger start files/scenes/player.gd
@@ -7,6 +7,18 @@ func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left","right","up","down") 
 	velocity = direction * speed
 	move_and_slide()
+	animation()
 	
 	if(Input.is_action_just_pressed("space")):
 		print("something")
+		
+func animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = 'left right'
+		else:
+			$AnimatedSprite2D.animation = 'up' if direction.y < 0 else 'down'
+	else:
+		$AnimatedSprite2D.frame = 0
+	
