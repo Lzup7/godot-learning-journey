c599aba6395ed17252456126eb56889c41951b26
Lucas Olvr. <lucasgamerzup@gmail.com>
2026-09-21 19:30:56 -0300

    aula9(layers_colisao)
    * **Aula 09: Collision Layers (Camadas de Colisão)**
      - Aprendi sobre o sistema de Layers e Masks de colisão na Godot.
      - Configurei as camadas de colisão para separar os diferentes elementos do jogo (como o jogador, os carros e o cenário).
      - Otimizei a física do jogo para garantir que as colisões ocorram apenas entre os objetos corretos, evitando interações indesejadas.

--- frogger start/frogger start files/scenes/game.gd
+++ frogger start/frogger start files/scenes/game.gd
@@ -14,4 +14,8 @@ func _on_car_timer_timeout() -> void:
 	var pos_marker = $CarStartPositions.get_children().pick_random() as Marker2D
 	car.position = pos_marker.position
 	$Objects.add_child(car)
+	car.connect("body_entered", go_to_title)
 	
+func go_to_title(body):
+	print(body)
+	print('player car collision')
